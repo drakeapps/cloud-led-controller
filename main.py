e80983f138aef9aa0480e5aa1d298a042f2af50a
James Wilson <james@drakeapps.com>
2020-11-22 16:43:00 -0600

    add more arg parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import argparse
+import inspect
 
 from AudioReactiveLEDStrip import visualization
 from AudioReactiveLEDStrip import led
@@ -7,32 +8,37 @@ from AudioReactiveLEDStrip import microphone
 
 
 def valid_argument(val):
-    if not val:
+    print(val)
+    if val:
         return val
     else:
         return None
+
+# take in the class/function 
+def build_arguments(klass, ags):
+    kv = {}
+    for arg in inspect.getfullargspec(klass).args:
+        if arg in ags and ags[arg]:
+            kv[arg] = ags[arg]
+    return kv
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Audio Reactive LED Tester')
     
     parser.add_argument('--n_pixels', type=valid_argument, dest='n_pixels')
+    
 
     args = parser.parse_args()
 
     print(args)
 
-    kv = {}
+    led_kv = build_arguments(led.LED, args)
 
-    for key in vars(args).keys():
-        if args[key] != None and args[key] != '':
-            kv[key] = args[key]
+    reactive_kv = build_arguments(visualization.AudioLEDVisualization, args)
 
-    print(kv)
-
-
-    leds = led.LED(
-        **kv
-    )
+    leds = led.LED(**led_kv)
     leds.update()
-    audio = visualization.AudioLEDVisualization(leds, effect="scroll")
+
+    audio = visualization.AudioLEDVisualization(leds, **reactive_kv)
+
     microphone.start_stream(audio.microphone_update)
